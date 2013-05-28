eede023e93ff5160ad3a031457647aec91b5f0fa
Fuji, Goro <g.psy.va@gmail.com>
2013-05-28 16:22:44 -0700

    set default location and highlight my location

--- src/com/example/googlemapv2example/MainActivity.java
+++ src/com/example/googlemapv2example/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.googlemapv2example;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 
-public class MainActivity extends Activity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+/**
+ * @see https://developers.google.com/maps/documentation/android/start
+ */
+public class MainActivity extends FragmentActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+
+		final Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.map);
+		final GoogleMap map = ((SupportMapFragment)fragment).getMap();
+		map.setMyLocationEnabled(true);
+
+		final LatLng mtFujiCoord = new LatLng(35.3580, 138.7310);
+		map.moveCamera( CameraUpdateFactory.newLatLngZoom(mtFujiCoord, 10.0f) );
 	}
 }
